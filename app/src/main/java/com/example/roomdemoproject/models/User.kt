56b8a7648ca018ecc5c06ea9c66b7b359ba3baa1
mmeacham92 <meacham.michael@rocketmail.com>
2023-11-13 15:26:26 -0600

    created User model class, imported dependencies seem good

--- app/src/main/java/com/example/roomdemoproject/models/User.kt
+++ app/src/main/java/com/example/roomdemoproject/models/User.kt
@@ -1,5 +1,14 @@
 package com.example.roomdemoproject.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity
-data class User {
-}
+data class User(
+    @PrimaryKey val id: Int,
+
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?,
+    @ColumnInfo(name = "phone_number") val phoneNumber: String?
+)
